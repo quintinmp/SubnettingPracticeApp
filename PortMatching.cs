8d86c2cdd122ee4b4590004706eca6631dbf0b3f
quintinmp <100251509+quintinmp@users.noreply.github.com>
2025-11-28 00:07:04 -0700

    added rest of ports and port nums and buttons for nums. started logic for matching

--- PortMatching.cs
+++ PortMatching.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 using System.Text.Json;
+using System.Linq;
 
 
 namespace WindowsFormsApp1
@@ -12,9 +13,13 @@ namespace WindowsFormsApp1
     {
         Form1 form1;
         Button[] buttons;
+        Button[] numButtons;
+        Dictionary<int, String> answers;
 
         private String font = "Calibri";
         private int fontSize = 15;
+
+        String answer1, answer2;
 
         public PortMatching(Form1 form1)
         {
@@ -24,26 +29,78 @@ namespace WindowsFormsApp1
         public void SetupPortMatching()
         {
             buttons = new Button[20];
-            SetupAllButtons(buttons);
+            numButtons = new Button[20];
+            SetupButtons(buttons);
+            SetupNumButtons(numButtons);
             LoadJson(buttons);
+            LoadNumJson(numButtons);
 
         }
 
-        private void SetupAllButtons(Button[] buttons)
+        private void SetupButtons(Button[] buttons)
         {
             for (int i = 0; i < buttons.Length; i++)
             {
                 buttons[i] = new Button();  // Create button instance
                 buttons[i].Font = new Font(font, fontSize);
-                buttons[i].Location = new Point(150 + (i % 5) * 100, 150 + (i / 5) * 50);
+                buttons[i].Location = new Point(150 + (i % 5) * 100, 60 + (i / 5) * 50);
                 buttons[i].AutoSize = true;
-                buttons[i].Tag = i;  // Store index for reference
+                buttons[i].Tag = new {Type = "name", Index = i};  // Store index for reference
+                buttons[i].Click += Button_Click;  // Use generic handler
+                form1.Controls.Add(buttons[i]);
+            }
+        }
+
+        private void SetupNumButtons(Button[] buttons)
+        {
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i] = new Button();  // Create button instance
+                buttons[i].Font = new Font(font, fontSize);
+                buttons[i].Location = new Point(150 + (i % 5) * 100, 325 + (i / 5) * 50);
+                buttons[i].AutoSize = true;
+                buttons[i].Tag = new { Type = "number", Index = i };  // Store index for reference
                 buttons[i].Click += Button_Click;  // Use generic handler
                 form1.Controls.Add(buttons[i]);
             }
         }
 
         private void Button_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+
+            if (clickedButton == null) return;
+
+            var tagData = (dynamic)clickedButton.Tag;
+            int index = tagData.Index;
+
+            if (tagData.Type == "name")
+            {
+                Console.WriteLine("you clicked a port name");
+                HandleNameClick(buttons[index], index);
+            }
+            else if (tagData.Type == "number")
+            {
+                Console.WriteLine("you clicked a port number");
+                HandleNumClick(numButtons[index], index);
+
+            }
+
+        }
+
+        private void HandleNameClick(Button button, int index)
+        {
+            if (answer1 == null)
+            {
+                buttons[index].Text = answer1;
+            } 
+            else
+            {
+                buttons[index].Text = answer2;
+            }
+        }
+
+        private void HandleNumClick(Button button, int index)
         {
 
         }
@@ -63,7 +120,23 @@ namespace WindowsFormsApp1
             {
                 buttons[i].Text = ports[i].name;
             }
+        }
 
+        private void LoadNumJson(Button[] btns)
+        {
+            string workingDir = Directory.GetCurrentDirectory();
+            Console.WriteLine($"working dir: {workingDir}");
+
+
+            string fileName = "ports.json";
+            string jsonContent = File.ReadAllText(fileName);
+
+            List<Port> ports = JsonSerializer.Deserialize<List<Port>>(jsonContent);
+
+            for (int i = 0; i < btns.Length && i < ports.Count; i++)
+            {
+                btns[i].Text = string.Join(", ", ports[i].portNum);
+            }
         }
 
 
